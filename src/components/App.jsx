32152a65bddc4203014f217f9a3da7627aa51f0e
aman24-cpu <buisnessatitsbest19@gmail.com>
2025-11-15 04:29:49 +0530

    components name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,45 @@
-import { useState } from 'react'
+import { use, useState } from 'react';
 import './App.css'
 import NavBar from './NavBar';
-import TextForms from './textforms';
-function App() {
-  const [count, setCount] = useState(0)
+import TextForms from './TextForms';
+import Alert from './alert';
+function App() { 
 
+const [mode, setmode] = useState('light'); // whether dark Mode is enabled or not
+const [alert, setalert] = useState(null)
+
+const showAlert = (message, type)=>{
+      setalert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+        setalert(null);
+      }, 1500);
+}
+
+const togglemode = ()=>{
+    if (mode==='light'){
+      setmode('dark')
+      document.body.style.backgroundColor = '#302A2A';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setmode('light')
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+  }
+}
   return (
     <>
-   <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">textutils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
-        </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+    <NavBar Title="TextUtils" mode={mode} togglemode={togglemode}>
+    </NavBar>
+    <Alert Alert= {alert}/>
+    <div className="container my-3"> 
+    <TextForms showAlert={showAlert} heading="Enter The Text to Analyze Below" mode={mode}/>  
+    
     </div>
-  </div>
-</nav>
-<NavBar></NavBar>
-<div className="container my-9"> 
-<TextForms heading="Enter The Text to Analyze Below"/>
-</div>
- </>
+    </>
 
 );
 }
--- src/components/TextForms.jsx
+++ src/components/TextForms.jsx
@@ -5,6 +5,19 @@ export default function TextForms(props) {
     console.log("Uppercase was clicked" + text);
     let newText = text.toUpperCase();
     setText(newText)
+    props.showAlert("Converted to uppercase!" , "success");
+  }
+  const handleLoClick = ()=>{
+    console.log("Lowercase was clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText)
+    props.showAlert("Converted to lowercase!" , "success");
+  }
+  const handleClearClick = ()=>{
+    console.log("Lowercase was clicked" + text);
+    let newText = ("");
+    setText(newText)
+    props.showAlert("Text cleared" , "success");
   }
 
   const handleOnchange = (event)=>{
@@ -12,15 +25,26 @@ export default function TextForms(props) {
     setText(event.target.value)
   }
   
-  const [text,setText] = useState('Enter text here2');
+  const [text,setText] = useState('');
   return (
-    <div>
-    <h1>{props.heading}</h1>
-      <div className="mb-3">
-      <textarea className="form-control" value={text} onChange={handleOnchange} id="MyText" rows="9"></textarea>
-      </div>
-    <button className="btn btn-primary" onClick={handleUpClick}>convert to uppercase</button>
+    <>
+    
+        <div className="container" style= {{color: props.mode==='dark'?'white':'#302A2A'}}>
+          <h1>{props.heading}</h1>
+          <div className="mb-3">
+          <textarea className="form-control" value={text} onChange={handleOnchange} style= {{backgroundColor: props.mode==='dark'?'grey':'white' , color: props.mode==='dark'?'white':'#302A2A'}} id="MyText" rows="9"></textarea>    </div>
+         <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase </button>
+         <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase </button>
+         <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear </button>
+       </div>
+    <div className="container my-3" style= {{color: props.mode==='dark'?'white':'#302A2A'}}>
+    <h1>Your text summary</h1>
+    <p>{text.split(" ").length} words and {text.lenght} characters</p>
+    <p>{0.008 * text.split(" ").length} Minutes read</p>
+    <h2>Preview</h2>
+    <p>{text}</p>
     </div>
+   </>
   )
 }
 
